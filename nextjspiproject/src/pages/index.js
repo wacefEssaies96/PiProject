c99d4b3989e04577da8329e429b8165b86deb763
krichen-Noureddine <noureddine.krichen@esprit.tn>
2023-03-01 10:45:51 +0100

    migrate typeScript to JS

--- nextjspiproject/src/pages/index.js
+++ nextjspiproject/src/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,7 +18,7 @@ export default function Home() {
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
+            <code className={styles.code}>src/pages/index.js</code>
           </p>
           <div>
             <a
