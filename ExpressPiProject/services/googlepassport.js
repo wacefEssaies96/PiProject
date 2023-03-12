5b4437ef08ddee8076bce90318d565020ee4c33c
WacefEssaies <wacef.essaies@esprit.tn>
2023-03-12 00:00:53 +0000

    AC-62 added linkedin auth

--- ExpressPiProject/services/googlepassport.js
+++ ExpressPiProject/services/googlepassport.js
@@ -1,25 +1,20 @@
-var passport = require("passport");
+const passport = require("passport");
 var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
-passport.serializeUser(function(user, done) {
- done(null, user);
+passport.serializeUser(function (user, done) {
+    done(null, user);
 });
-passport.deserializeUser(function(user, done) {
- done(null, user);
+passport.deserializeUser(function (user, done) {
+    done(null, user);
 });
-passport.use(
- new GoogleStrategy(
-  {
-   clientID: process.env.CLIENT_ID,
-   clientSecret: process.env.CLIENT_SECRET,
-   callbackURL: `http://localhost:3030/auth/google/callback`
-  },
-  function(accessToken, refreshToken, profile, done) {
-   var userData = {
-    email: profile.emails[0].value,
-    name: profile.displayName,
-    token: accessToken
-   };
-   done(null, userData);
-  }
- )
-);
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: `http://localhost:3030/auth/google/callback`
+}, function (accessToken, refreshToken, profile, done) {
+    var userData = {
+        email: profile.emails[0].value,
+        name: profile.displayName,
+        token: accessToken
+    };
+    done(null, userData);
+}));
